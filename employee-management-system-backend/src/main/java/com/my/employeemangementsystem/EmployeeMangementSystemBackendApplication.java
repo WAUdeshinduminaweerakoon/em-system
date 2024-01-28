87fe49c9286c3cfd06f4b518b86c0a5ff46f82ec
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-01-28 08:55:07 +0530

    Creating an Employee-Management-System, Spring Project

---
+++ employee-management-system-backend/src/main/java/com/my/employeemangementsystem/EmployeeMangementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.my.employeemangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangementSystemBackendApplication.class, args);
+	}
+
+}
