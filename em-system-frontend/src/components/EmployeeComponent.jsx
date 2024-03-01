3935e8f316445662af3aff1d086105a8d8d9c365
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-03-01 10:49:08 +0530

    Add Employee Form Handling in React App

--- em-system-frontend/src/components/EmployeeComponent.jsx
+++ em-system-frontend/src/components/EmployeeComponent.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const EmployeeComponent = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    
+    function saveEmployee(e){
+        e.preventDefault();
+
+        const employee = {firstName, lastName, email}
+        console.log(employee)
+    }
+
   return (
-    <div>
-      <h1>Add Employee</h1>
+    <div className="flex justify-center items-center h-screen">
+    <div className="w-full max-w-xs">
+        
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h2 className="block text-gray-700 text-xl font-bold mb-4 ">Add Employee</h2>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                    First Name
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Enter Employee First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={(e)=>setFirstName(e.target.value)}
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Last Name
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Enter Employee Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange={(e)=> setLastName(e.target.value)}
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Email
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Enter Employee Email"
+                    name="email"
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                />
+            </div>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                type="button"
+                onClick={saveEmployee}
+                >
+                Submit
+            </button>
+        </form>
     </div>
+</div>
   )
 }
 
